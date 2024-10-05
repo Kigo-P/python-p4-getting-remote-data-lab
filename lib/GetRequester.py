6abd9418b9600f7c32914df83caaf3780c7e5d6d
Kigo-P <peterkigo33@gmail.com>
2024-10-05 13:48:00 +0300

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # # creating a variable that contains the url endpoint
+        # url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+        # using get method to create a response based on the url
+        response = requests.get(self.url)
+        #  returning the response.content
+        print(response.content)
+        return(response.content)
+    
 
     def load_json(self):
-        pass
+        # creating a variable that uses json.loads() to convert json to python
+        json_content = json.loads(self.get_response_body())
+        # returning the data 
+        return json_content
+        
